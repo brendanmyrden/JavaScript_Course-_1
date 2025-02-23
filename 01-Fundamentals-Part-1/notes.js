3df6d1a1ff748a4fd002b051084f6cd36d7fabd8
MYRDEN <79550484+brendanmyrden@users.noreply.github.com>
2025-02-23 14:46:48 -0400

    Initial creation of files, alerts, and math

---
+++ 01-Fundamentals-Part-1/notes.js
@@ -0,0 +1,50 @@
+// Fundamentals
+
+// In console :
+// a. command + option + J
+// b. left click + inspect
+// c. Broswer header + view + developper + JavaScript Console
+
+// Alerts, Let / If
+
+// 1
+
+let js = "amazing";
+
+if (js === "amazing")
+  alert("Brendan Wake Up. This Is The Time Created For You To Shine.");
+
+js = "boring";
+if (js === "amazing")
+  alert("Brendan Wake Up. This Is The Time Created For You To Shine.");
+
+if (js === "amazing")
+  alert("Brendan Wake Up. This Is The Time Created For You To Shine.");
+
+// 2
+
+let question = "Why have you always wondered what is possible?";
+
+if (question === "Why have you always wondered what is possible?")
+  alert("The answer is within you.");
+
+// 3
+
+let truth = "You are the child of the Heavenly King";
+if (truth === "You are the child of the Heavenly King") alert("Go All In");
+
+// 4
+
+let js = "amazing";
+if (js === "amazing") alert("The best is yet to come!");
+
+// If placed within an index.html file it's called an 'in-line script'
+
+// Maths (In Console)
+
+40 + 8 + 23 - 10;
+
+10 + 12 + 54 + 2 - 4 / 6;
+
+40 + 8 + 23 - 10;
+console.log(40 + 8 + 23 - 10);
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,12 @@
+// Fundamentals
+
+// Hello World
+
+let hello = "Hello World, Here I am";
+
+if (hello === "Hello World, Here I am") alert("You are here on the Earth!");
+
+// Math
+
+20 + 40;
+console.log(40 + 20);
